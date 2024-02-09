769ecc9867e6969ff5ab2cd9be50af9f917a7668
gev414 <129388854+gev414@users.noreply.github.com>
2024-02-09 13:47:52 +0200

    Reordered checks so an X will show where carrots were "eaten" before drawing matrix on the console

--- src/Main.java
+++ src/Main.java
@@ -14,12 +14,33 @@ public class Main {
                     garden[i][j] = random.nextInt(2);
                 }
             }
-
-            // draw
+                
+            // iterates over every index, as well as i+1 and if condition sets all to 0 (eaten)
+        
+            int leaps = 0;
+            for (int i = 0; i < garden.length - 1; i++) {
+                for (int j = 0; j < garden.length - 1; j++) {
+                    if (garden[i][j] < garden.length) {
+                        if (garden[i][j] == 1 && garden[i][j + 1] == 1 &&
+                                garden[i + 1][j] == 1 && garden[i + 1][j + 1] == 1) {
+                            garden[i][j] = 3;
+                            garden[i][j+1] = 3;
+                            garden[i+1][j] = 3;
+                            garden[i+1][j+1] = 3;
+                            leaps++;
+                        }
+                    }
+                }
+            }        
+            
+            
+            // draw - " | " where carrots are, " X " where carrots where eaten
             for (int i = 0; i < garden.length; i++) {
                 for (int j = 0; j < garden.length; j++) {
                     if (garden[i][j] == 1) {
                         System.out.print(" | ");
+                    } else if(garden[i][j]==3){
+                        System.out.print(" X ");
                     } else {
                         System.out.print(" O ");
                     }
@@ -27,22 +48,6 @@ public class Main {
                 System.out.println();
             }
 
-           // iterates over every index, as well as i+1 and if condition sets all to 0 (eaten)
-            int leaps = 0;
-            for (int i = 0; i < garden.length - 1; i++) {
-                for (int j = 0; j < garden.length - 1; j++) {
-                    if (garden[i][j] < garden.length) {
-                        if (garden[i][j] == 1 && garden[i][j + 1] == 1 &&
-                                garden[i + 1][j] == 1 && garden[i + 1][j + 1] == 1) {
-                            garden[i][j] = 0;
-                            garden[i][j+1] = 0;
-                            garden[i+1][j] = 0;
-                            garden[i+1][j+1] = 0;
-                            leaps++;
-                        }
-                    }
-                }
-            }
 
             System.out.println("Number of leaps: " + leaps);
         }
